2b6e57bc06faa35ed04e53bee1f1cf671d8f5108
wyn <k022298@gmail.com>
2020-02-20 13:21:37 +0800

    till lesson 23

--- bullseyeLearning/bullseyeLearning/ContentView.swift
+++ bullseyeLearning/bullseyeLearning/ContentView.swift
@@ -3,62 +3,62 @@ import SwiftUI
 struct ContentView: View {
     
     @State var alertIsVisible: Bool = false
-    @State var whosThereIsVisible: Bool = false
-    
+    @State var sliderValue: Double = 50.0
+    @State var target:Int = Int.random(in: 1...100)
     
     var body: some View {
         VStack {
+            Spacer()
             HStack {
                 Text("Put the Bullseye as close as you can to:")
-                Text(/*@START_MENU_TOKEN@*/"100"/*@END_MENU_TOKEN@*/)
+                Text("\(self.target)")
             }
-//            HStack{
-//                Text("1")
-////                Slider(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(10)/*@END_MENU_TOKEN@*/)
-//                Text("100")
-//            }
-//            Button(action: {
-//                print("Button Press!")
-//                self.alertIsVisible = true
-//            }) {
-//                Text("Hit me")
-//            }
-//            HStack{
-//                Button(action:{
-////                print("press Start Over!")
-//                }){
-//                    Text("Start over")
-//                }
-//                Text("Score:")
-//                Text("999999")
-//                Text("Round:")
-//                Text("999")
-//                Button(action:{
-////                    print("press Info")
-//                })
-        }
-//            .alert(isPresented: $alertIsVisible){ ()->
-//             Alert in
-//                return Alert(title: Text("Hello there!"), message: Text("This is my first pop-up."), dismissButton: .default(Text("Awesome!")))
-//            }
-            
-            //Score row
-//            Button(action: {
-//                self.whosThereIsVisible = true
-//            }) {
-//                Text(/*@START_MENU_TOKEN@*/"Knock, knock"/*@END_MENU_TOKEN@*/)
-//            }
-//            .alert(isPresented: $whosThereIsVisible){ () ->
-//            Alert in
-//                return Alert(title: Text("Who is there?"), message: Text("Little old boy."), dismissButton: .default(Text("Little old boy who?")))
-////            }
-//        }
-//    }
+            Spacer()
+            HStack{
+                Text("1")
+                Slider(value: self.$sliderValue, in:1...100)
+                Text("100")
+            }
+            Button(action: {
+                print("Button Press!")
+                self.alertIsVisible = true
+            }) {
+                Text("Hit me")
+            }
+            .alert(isPresented: $alertIsVisible){ ()->
+            Alert in
+               return Alert(title: Text("Hello there!"), message: Text("The slider's value is \(self.sliderValue.rounded()).\n" +
+                "scored \(self.pointsForCurrentRound()) points this round."
+               ), dismissButton: .default(Text("Awesome!")))
+                                               }
+            Spacer()
+            HStack{
+                Button(action:{
+                print("press Start Over!")
+                }){
+                    Text("Start Over")
+                }
+                Spacer()
+                Text("Score:")
+                Text("999999")
+                Spacer()
+                Text("Round:")
+                Text("999")
+                Spacer()
+                Button(action:{
+                    print("press Info")
+                    }){ Text("Info") }
+                    }
+            .padding(.bottom,20)
+            }
 }
-
+    func pointsForCurrentRound()->Int{
+        return 999
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().previewLayout(.fixed(width: 896, height: 414 ))
     }
 }
-}
+
